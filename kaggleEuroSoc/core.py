f2304c7a6b154f0cf5a1836b2162d59c6ba56dd8
Alan Clark <alan@thatscottishdatascientist.com>
2019-01-06 19:22:48 +0000

    Adding prompts for db details

--- kaggleEuroSoc/core.py
+++ kaggleEuroSoc/core.py
@@ -1,3 +1,5 @@
+import getpass
+
 from kaggleEuroSoc.models.country import Country
 from kaggleEuroSoc.models.league import League
 from kaggleEuroSoc.models.match import Match
@@ -9,9 +11,19 @@ from kaggleEuroSoc.helpers.database import create_session
 
 
 def main():
+    print('Please provide the path of the SQLite database')
+    sqlite_path = input('SQLite path [database.sqlite]: ') or 'database.sqlite'
+
+    print('Please provide the host, port, database name, username and password for the PostgreSQL database')
+    host = input('Host [localhost]: ') or 'localhost'
+    port = input('Port [5432]: ') or 5432
+    database = input('Database: ')
+    username = input('Username: ')
+    password = getpass.getpass('Password: ')
+
     print('Connecting to databases')
-    sqllite_session = create_session('sqlite:///database.sqlite')
-    postgres_session = create_session('postgresql+psycopg2://username:password@localhost:5432/database', init=True)
+    sqllite_session = create_session(f'sqlite:///{sqlite_path}')
+    postgres_session = create_session(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}', init=True)
 
     print('Beginning migration')
     migrate(Country, sqllite_session, postgres_session)
